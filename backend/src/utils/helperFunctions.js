37798af4a1a46532686d8361dd84feb83243a183
oschwill <o.schwill@web.de>
2024-04-11 12:06:58 +0200

    registration complete / send Token, complete register / resend Token functionality

--- backend/src/utils/helperFunctions.js
+++ backend/src/utils/helperFunctions.js
@@ -1,1 +1,11 @@
+import fs from 'fs/promises';
 
+export const createEmailTemplate = async (template, input) => {
+  let htmlTemplate = await fs.readFile(template, 'utf8');
+
+  input.forEach((item) => {
+    htmlTemplate = htmlTemplate.replace(item.placeholder, item.data);
+  });
+
+  return htmlTemplate;
+};
