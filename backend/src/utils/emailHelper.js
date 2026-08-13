ec0fcb590b8059b599a6239bcf306086a941b965
Oliver Schwill <o.schwill@web.de>
2026-08-13 09:01:19 +0200

    Additional Email Sender Customizations

--- backend/src/utils/emailHelper.js
+++ backend/src/utils/emailHelper.js
@@ -7,19 +7,30 @@ export const sendDynamicEmail = async (options) => {
     host: process.env.EMAIL_HOST,
     port: Number(process.env.EMAIL_PORT),
     secure: Number(process.env.EMAIL_PORT) === 465,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
+    ...(isProd && {
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    }),
   });
 
-  const info = await transporter.sendMail({
-    from: `My Wiki <${process.env.EMAIL_FROM}>`,
-    to: options.email,
-    subject: options.subject,
-    text: options.text, // plain text body
-    html: options.html, // html body
-  });
+  try {
+    const info = await transporter.sendMail({
+      from: `My Wiki <${process.env.EMAIL_FROM}>`,
+      to: options.email,
+      subject: options.subject,
+      text: options.text,
+      html: options.html,
+    });
+
+    console.log('Message sent:', info.messageId);
+
+    return true;
+  } catch (error) {
+    console.error('EMAIL ERROR:', error);
+    throw error;
+  }
 
   console.log('Message sent: %s', info.messageId);
 
